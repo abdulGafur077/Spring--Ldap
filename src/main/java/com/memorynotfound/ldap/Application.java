6ab1e3149ec379d174ed05d2d41076c17521f37a
Abdul Gafoor <abdul@shrigowri.com>
2018-02-16 16:04:18 +0530

    First commit

---
+++ src/main/java/com/memorynotfound/ldap/Application.java
@@ -0,0 +1,18 @@
+package com.memorynotfound.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@SpringBootApplication
+// Enable if you want to use Spring Security + LDAP Authentication XML Configuration
+ @ImportResource("classpath:spring-security-config.xml") 
+public class Application {
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
